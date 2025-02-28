ad51304279bd640fc0b33846d02fe1d44df350b8
sefa <sefasoysal@protonmail.com>
2025-02-28 17:18:00 +0300

    responsive design ve birkaç düzeltme yapıldı

--- shop-page/script.js
+++ shop-page/script.js
@@ -1,10 +1,14 @@
 const counter = document.getElementById("counter");
+
 let count = 0;
 document.addEventListener("DOMContentLoaded", () => {
   const addIcons = document.querySelectorAll(".ri-add-line"); 
   addIcons.forEach((addIcon) => {
     addIcon.addEventListener("click", () => {
-      count++
+      if(count < 9)
+        count++;
+      if(count == 9)
+        counter.style.backgroundColor ="#ff0000"; 
       counter.textContent = count;
     });
   });
